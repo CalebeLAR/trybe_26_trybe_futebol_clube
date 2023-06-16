e8f3fb15b3da9423739b84cf97fbf3791190fb08
calebeLAR <ca6ele@gmail.com>
2023-06-16 17:44:30 -0400

    req03: cria e tipa TeamService com metodo findAll

---
+++ app/backend/src/services/TeamService.ts
@@ -0,0 +1,15 @@
+import TeamModel from '../models/TeamModel';
+import { ServiceResponse } from '../Interfaces/IServiseResponse';
+import { ITeam } from '../Interfaces/teams/ITeam';
+
+class TeamService {
+  private teamModel = new TeamModel();
+
+  async findAll(): Promise<ServiceResponse<ITeam[]>> {
+    const data = await this.teamModel.findAll();
+
+    return { status: 'SUCCESSFUL', data };
+  }
+}
+
+export default TeamService;
