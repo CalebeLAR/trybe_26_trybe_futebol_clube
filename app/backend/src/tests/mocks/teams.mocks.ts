6cf419363d661a6c5563d6241de3060099d3268b
calebeLAR <ca6ele@gmail.com>
2023-06-16 15:26:25 -0400

    req02: cria testes de integração para o endpoint GET /teams

---
+++ app/backend/src/tests/mocks/teams.mocks.ts
@@ -0,0 +1,22 @@
+const  teams = [
+  {
+    id: 1,
+    teamName: 'Avaí/Kindermann',
+  },
+  {
+    id: 2,
+    teamName: 'Bahia',
+  },
+  {
+    id: 3,
+    teamName: 'Botafogo',
+  },
+  {
+    id: 4,
+    teamName: 'Corinthians',
+  },
+]
+
+export default {
+  teams
+};
---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,31 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import SequelizeTeam from '../database/models/SequelizeTeam';
+import mockTeams from './mocks/teams.mocks';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('TEAMS tests', async function () {
+  beforeEach(function () { sinon.restore(); });
+
+  it('testa endpoint GET /teams ', async function () {
+    // arrange
+    const teams = mockTeams.teams;
+    sinon.stub(SequelizeTeam, 'findAll').resolves(teams as SequelizeTeam[])
+
+    // act
+    const httpResponse = await chai.request(app).get('/teams').send();
+
+    // assert
+    expect(httpResponse.status).to.be.eq(200);
+    expect(httpResponse.body.data).to.be.eq(teams)
+  });
+});
