e17a5aa0a97843bf5364e9bcbd151a9a318cf970
calebeLAR <ca6ele@gmail.com>
2023-06-18 09:39:09 -0400

    req04: cria metodo findByPk em teamController

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from 'express';
 import TeamService from '../services/TeamService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 export default class TeamController {
   private teamService = new TeamService();
 
   async findAll(_req:Request, res:Response):Promise<Response> {
-    const { data } = await this.teamService.findAll();
+    const { status, data } = await this.teamService.findAll();
 
-    return res.status(200).json(data);
+    return res.status(mapStatusHTTP(status)).json(data);
+  }
+
+  async findByPk(req:Request, res:Response):Promise<Response> {
+    const id = Number(req.params.id);
+    const { status, data } = await this.teamService.findByPk(id);
+    if (status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(status)).json(data);
+    }
+
+    return res.status(mapStatusHTTP(status)).json(data);
   }
 }
 
