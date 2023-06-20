6d9917c0e07ea43b677ecd2e3eba1bd9a548d250
calebeLAR <ca6ele@gmail.com>
2023-06-20 14:23:58 -0400

    req13: cria sequelizeMatch

---
+++ app/backend/src/database/models/SequelizeMatch.ts
@@ -0,0 +1,72 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeMatch extends Model<
+InferAttributes<SequelizeMatch>,
+InferCreationAttributes<SequelizeMatch>
+> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+SequelizeMatch.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  homeTeamId: {
+    field: 'home_team_id',
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  },
+  homeTeamGoals: {
+    field: 'home_team_goals',
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    field: 'away_team_id',
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  },
+  awayTeamGoals: {
+    field: 'away_team_goals',
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    field: 'in_progress',
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default SequelizeMatch;
