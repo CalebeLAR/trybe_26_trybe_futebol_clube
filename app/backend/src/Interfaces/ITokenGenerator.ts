5dd468ff48182f5ae017d7284fb78f933f6153e8
calebeLAR <ca6ele@gmail.com>
2023-06-20 10:54:56 -0400

    req12: adiciona verifyToken no gerador de tokens

--- app/backend/src/Interfaces/ITokenGenerator.ts
+++ app/backend/src/Interfaces/ITokenGenerator.ts
@@ -1,3 +1,4 @@
 export interface ITokenGenerator {
   gerToken<T extends object>(payload:T):string;
+  verifyToken<T extends object>(token:string):(T | null);
 }
--- app/backend/src/services/TokenGenerateJWT.ts
+++ app/backend/src/services/TokenGenerateJWT.ts
@@ -8,4 +8,14 @@ export default class TokenGenerator implements ITokenGenerator {
   gerToken<T extends object>(payload: T):string {
     return this.jwt.sign(payload, this.jwtSecret, { algorithm: 'HS256', expiresIn: '1h' });
   }
+
+  verifyToken<T extends object>(token: string): (T | null) {
+    try {
+      const payload = jwt.verify(token, this.jwtSecret);
+
+      return payload as T;
+    } catch (jwtErr) {
+      return null;
+    }
+  }
 }
