c87b855aba541f41a2b4f6c30e85e8aa9a3a7dd1
calebeLAR <ca6ele@gmail.com>
2023-06-20 14:23:39 -0400

    req13: cria interfaces para as tabelas e models referentes à matches

--- app/backend/src/Interfaces/ICRUDModel.ts
+++ app/backend/src/Interfaces/ICRUDModel.ts
@@ -4,3 +4,7 @@ export interface ICRUDModelReader<T> {
   findAll(): Promise<T[]>,
   findById(id: ID): Promise<T | null>,
 }
+
+export interface ICRUDModelUpdate {
+  update(id: ID):Promise<boolean>
+}
---
+++ app/backend/src/Interfaces/matches/IMatchModel.ts
@@ -0,0 +1,6 @@
+import { ICRUDModelReader, ICRUDModelUpdate } from '../ICRUDModel';
+import { IMatch } from './IMatch';
+
+export interface IMatchModel extends ICRUDModelReader<IMatch>, ICRUDModelUpdate {
+
+}
