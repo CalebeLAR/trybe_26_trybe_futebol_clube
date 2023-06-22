72ac41b259491752d6ea5a2234bd044c18a23a0d
calebeLAR <ca6ele@gmail.com>
2023-06-21 23:20:43 -0400

    refat: comenta metodo inutilizado

--- app/backend/src/services/EncrypterBcrypt.ts
+++ app/backend/src/services/EncrypterBcrypt.ts
@@ -4,10 +4,10 @@ import { IEncrypeter } from '../Interfaces/IEncrypter';
 export default class Encrypet implements IEncrypeter {
   private bcrypt = bcrypt;
 
-  async encrypt(password: string): Promise<string> {
-    const hash = await this.bcrypt.hash(password, 10);
-    return hash;
-  }
+  // async encrypt(password: string): Promise<string> {
+  //   const hash = await this.bcrypt.hash(password, 10);
+  //   return hash;
+  // }
 
   async compare(password: string, hash: string): Promise<boolean> {
     const isValid = await this.bcrypt.compare(password, hash);
