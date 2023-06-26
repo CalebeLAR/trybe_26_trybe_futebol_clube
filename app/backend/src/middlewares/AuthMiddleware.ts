a8609cb950d29c28c5ff1499a086435f33b0fc4e
calebeLAR <ca6ele@gmail.com>
2023-06-25 21:01:55 -0400

    refat: reescreve AutMiddleware

--- app/backend/src/middlewares/AuthMiddleware.ts
+++ app/backend/src/middlewares/AuthMiddleware.ts
@@ -1,13 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import { IUser } from '../Interfaces/users/IUser';
+import TokenGenerator from '../services/TokenGenerateJWT';
 
 export default class AuthMiddleware {
-  static validateToken(req:Request, res:Response, next:NextFunction) {
+  static tokenGenerator = new TokenGenerator();
+  static validateUserToken(req:Request, res:Response, next:NextFunction) {
     const { authorization: token } = req.headers;
 
     if (!token) {
       return res.status(401).json({ message: 'Token not found' });
     }
 
+    const payload = AuthMiddleware.tokenGenerator.verifyToken<IUser>(token);
+
+    if (!payload) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
+
+    req.body.payload = payload;
     return next();
   }
 }
